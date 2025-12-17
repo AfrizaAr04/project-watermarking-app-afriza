a9c126caac7e344a6413a6f21da1d9ecc74d3e55
AfrizaAr04 <afririza999@gmail.com>
2025-12-18 06:38:44 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -6,15 +6,18 @@ import io
 import base64
 
 app = Flask(__name__)
-CORS(app)
+# Buka akses untuk Vercel
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/')
 def home():
-    return jsonify({"status": "ready", "message": "Backend di folder backend/ aman!"})
+    return jsonify({"status": "ready", "message": "Backend di folder backend aman!"})
 
-@app.route('/process-image', methods=['POST'])
+@app.route('/process-image', methods=['POST', 'OPTIONS'])
 def process():
-    return jsonify({"message": "Berhasil"})
+    return jsonify({"message": "Berhasil terhubung!"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
+    # Railway menggunakan port dinamis
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
