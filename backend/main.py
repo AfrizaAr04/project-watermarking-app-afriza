34878ad9e7a8ee2da93ce7355b87f28213b23c33
AfrizaAr04 <afririza999@gmail.com>
2025-12-18 07:17:30 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -7,12 +7,7 @@ import base64
 
 app = Flask(__name__)
 
-# Sekarang 'CORS' sudah didefinisikan karena diimport di atas
-CORS(app, resources={r"/*": {
-    "origins": "*",  # Sementara pakai bintang dulu untuk memastikan jalan
-    "methods": ["GET", "POST", "OPTIONS"],
-    "allow_headers": ["Content-Type", "Authorization"]
-}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/')
 def home():
